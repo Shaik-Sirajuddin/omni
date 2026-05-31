41adb9266ea7c57c233d11e7bf480dc4893c833f
Shaik-Sirajuddin <sirajuddinshaik30@gmail.com>
2026-05-31 15:24:42 +0530

    docs: remove docusaurus attribution from footer

--- docsite/docusaurus.config.ts
+++ docsite/docusaurus.config.ts
@@ -177,7 +177,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Omni Project. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Omni Project.`,
     },
     prism: {
       theme: prismThemes.github,
